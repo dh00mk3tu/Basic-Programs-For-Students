7cd85d68128ecbfe25d888e373dd1907e64db90f
dh00mk3tu <cyberhybird@hotmail.com>
2020-08-22 02:43:56 +0530

    How to use len() Function

--- Python/ArrLen.py
+++ Python/ArrLen.py
@@ -1,5 +1,6 @@
-# Made By Vanshika Lal
+# Made By Anirudh Rath | dh00mk3tu
+# Visit anirudhrath.tech to know more
 # Python program to demonstrate working 
-# of len() 
+# of len() function 
 n = len([10, 20, 30]) 
 print("The length of list is: ", n) 
