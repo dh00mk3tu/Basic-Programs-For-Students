174d68f7f94c02eddf8125880808038915145c2b
dh00mk3tu <cyberhybird@hotmail.com>
2020-08-22 03:15:16 +0530

    Multiple of 5 or not

--- Python/MultipleOf5.py
+++ Python/MultipleOf5.py
@@ -1,3 +1,7 @@
+# Made By Anirudh Rath | dh00mk3tu
+# Visit anirudhrath.tech to know more
+# Find if the number is Multiple of 5 
+
 num = int(input("Enter a number: "))
 if ((num % 5) == 0 or (num % 5) == 5):
    print(num, "Multiple of 5")
