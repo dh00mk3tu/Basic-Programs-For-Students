cfe60c839690f4d1eca1ab9c9a0526534d19ab87
dh00mk3tu <cyberhybird@hotmail.com>
2020-08-22 03:18:41 +0530

    Odd/Even check on input number

--- Python/OddEven.py
+++ Python/OddEven.py
@@ -1,3 +1,7 @@
+# Made By Anirudh Rath | dh00mk3tu
+# Visit anirudhrath.tech to know more
+# Check if input number is odd or even 
+
 num = int(input("Enter a number: "))
 if (num % 2) == 0:
    print(num, "is Even")
