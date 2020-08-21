591c3947d2547d8444e348c1b999abedec061645
dh00mk3tu <cyberhybird@hotmail.com>
2020-08-22 03:23:24 +0530

    Simple Interest in Python

--- Python/SimpleInterest.py
+++ Python/SimpleInterest.py
@@ -1,3 +1,7 @@
+# Made By Anirudh Rath | dh00mk3tu
+# Visit anirudhrath.tech to know more
+# Calculate Simple interest in Python
+
 a = float(input("Enter the principal amount : "))
 b = float(input("Enter the number of years : "))
 c = float(input("Enter the rate of interest : "))
